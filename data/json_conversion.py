18330f3e0c59da4950eee00ddb1895d65c778604
matildepassafaro <matilde__passafaro@outlook.it>
2024-07-30 09:23:44 +0100

    updated item page, bg images, updated js code

--- data/json_conversion.py
+++ data/json_conversion.py
@@ -1,16 +1,20 @@
 import json
 
+# read json file
 with open("data\data.json", mode="r", encoding="utf-8") as file:
-    json_object = json.loads(file.read())
-
+    json_object = json.load(file)
+    #print(json_object)
     #print(json_object[0])
     #print(len(json_object)) #18
 
+# initialize new dict structure
 updated_structure = {
     "items": []
 }
 #keys = updated_structure.keys()
 #print(keys)
+
+# initialize nested dictionary
 item = {
     "name": "",
     "info": {
@@ -22,42 +26,25 @@ item = {
         "artistic expression": ""
     }
 }
-#def update_structure(json_object):
-    #for i in json_object: 
-        #updated_structure["items"].append(item)
-        #for dict in updated_structure["items"]:
-            #print(dict)
-            #dict = {
-                #"name": i["name"],
-                #"info": {
-                    #"date": i["date"],
-                    #"conservation location": i["conservation location"],
-                    #"author": i["author"],
-                    #"authority": i["authority"],
-                    #"place": i["place"],
-                    #"art": i["art"],
-                    #"religion": i["religion"],
-                    #"image": i["image"],
-                    #"text 1": i["text 1"],
-                    #"text 2": i["text 2"],
-                    #"text 3": i["text 3"],
-                    #"narratives": {
-                        #"time": i["date"],
-                        #"place": i["place"],
-                        #"artistic expression": i["art"]
-                    #}
-                #}   
-            #}
-            #updated_structure["items"].append(dict)
-    #print(updated_structure)  
-    #return updated_structure
 
+# function definition
 def update_structure(json_object):
     for i in json_object:
-        print(i)
         item = {
             "name": i["name"],
+            "iId": i["iId"],
             "info": {
+                "text 1": i["text 1"],
+                "text 2": i["text 2"],
+                "text 3": i["text 3"],
+                "image": i["image"],
+                "narratives": {
+                    "time": i["date"],
+                    "place": i["place"],
+                    "artistic expression": i["art"]
+                    }
+            },
+            "itemMeta": {
                 "date": i["date"],
                 "conservation location": i["conservation location"],
                 "author": i["author"],
@@ -65,27 +52,21 @@ def update_structure(json_object):
                 "place": i["place"],
                 "art": i["art"],
                 "religion": i["religion"],
-                "image": i["image"],
-                "text 1": i["text 1"],
-                "text 2": i["text 2"],
-                "text 3": i["text 3"],
-                "narratives": {
-                    "time": i["date"],
-                    "place": i["place"],
-                    "artistic expression": i["art"]
-                    }
-                }   
-            }
+            },  
+        }
         updated_structure["items"].append(item)
     #print(updated_structure["items"])
     return updated_structure
 
+# function call
 new_file = update_structure(json_object)
-print("This is the new file:")
-print(new_file)
+#print("This is the new file:")
+#print(new_file)
 #print(len(updated_structure["items"]))
 
+
+# output json
 json_string = json.dumps(new_file)
 
-#with open("updated_structure.json", mode="w", encoding="utf-8") as file:
-    #file.write(json_string, indent=4)
+with open("data\\revised_structure.json", mode="w", encoding="utf-8") as output:
+    json.dump(new_file, output, ensure_ascii=False, indent=4)
--- script.js
+++ script.js
@@ -2,18 +2,145 @@ var items = [];
 var narratives = [];
 var selection = [];
 var curNarrative = "";
-var curValue = "";
+var curVal = "";
 var curSort = "";
 
 document.addEventListener("DOMContentLoaded", async function() {
     console.log("Ready to fetch")
-    fetch("data/data.json")
-    .then(response => response.json())
-    .then(data => {
-        items = data.name;
-        var itemIdx = data.meta.itemIdx;
-        var item = items[data.meta.itemIdx];
-        console.log("Names stored")
-        console.log(names)
+    $.ajax({
+        url: "data/revised_structure.json",
+        method: "get",
+        success: function(data) {
+            //console.log("Data loaded");
+            items = data.items;
+            console.log("items stored")
+            //console.log(JSON.stringify(items[0]))
+            var startItem = data.meta.startItem;
+            var item = items[startItem];
+            console.log(JSON.stringify(item))
+            narratives = data.meta.narratives;
+            //console.log(JSON.stringify(narratives))
+            curNarrative = data.meta.startNarrative;
+            //console.log(JSON.stringify(curNarrative))
+            curVal = data.meta.startVal;
+            //console.log(JSON.stringify(curVal))
+            prepareNarratives()
+        },
+        error: function(data) {
+            alert("Loading error");
+        }
     })
-})
+});
+
+function prepareNarratives() {
+    //alert("preparing narratives")
+    currentSelection = items.filter(i => i.info.narratives[curNarrative] == curVal)
+    console.log(JSON.stringify(currentSelection))
+
+currentSelection.sort((i,j) => {
+    if (i["iId"] < j["iId"]) return -1;
+    if (i["iId"] > j["iId"]) return 1;
+    return 0;
+});
+if (currentSelection.length==0)
+    currentSelection = items
+
+var index = currentSelection.findIndex(i => i["iId"] == curSort) // possible error here. The findIndex function evalutes to -1, so the first item in the array is always displayed
+if (index == -1) index = 0
+showInfo(index)
+};
+
+function showInfo(index) {
+    var item = currentSelection[index]
+    console.log(JSON.stringify(item))
+    curSort = item["iId"] // there might be a problem here
+    document.getElementById("infoTitle").innerHTML = item.name;
+    document.getElementById("text1").innerHTML = item.info["text 1"] + '<a type="button" class="btn btn-outline-dark btn-sm" onclick="showText2()">Read more</a>';
+    document.getElementById("img").src = item.info.image;
+    document.getElementById("img").alt = item.name;
+    document.getElementById("item-figcaption").innerHTML = item.name;
+    document.getElementById("text2").innerHTML = item.info["text 2"] + '<a type="button" class="btn btn-outline-dark btn-sm" onclick="showText1()">Back</a> <a type="button" class="btn btn-outline-dark btn-sm" onclick="showText3()">Read more</a>';
+    document.getElementById("text3").innerHTML = item.info["text 3"] + '<a type="button" class="btn btn-outline-dark btn-sm" onclick="showText1()">Back</a>';
+    createInfoTable(item)
+
+    prepareNavigationButtons(index)
+
+    }
+
+function showText1() {
+    $("#text1").removeClass("d-none");
+    $("#text3").addClass("d-none");
+    $("#text2").addClass("d-none");
+    window.scrollTo(0,0)
+
+}
+
+function showText2() {
+    $("#text1").addClass("d-none");
+    $("#text3").addClass("d-none");
+    $("#text2").removeClass("d-none");
+    window.scrollTo(0,0)
+}
+
+function showText3() {
+    $("#text1").addClass("d-none");
+    $("#text2").addClass("d-none");
+    $("#text3").removeClass("d-none");
+    window.scrollTo(0,0)
+    
+}
+
+
+function createInfoTable(item) {
+    var table = $("#info");
+    table.html("");
+
+    for (let i in item.itemMeta) {
+        //alert(JSON.stringify(i)) // i gets the property
+        // in the following line item.info[i] gets the value
+        if (item.itemMeta[i]) {
+            let val = item.itemMeta[i];
+            if (narratives.includes(item.itemMeta[i])) {
+                val = ('<a class="button" role="button" href="#" onclick="changeNarrative(\''+i+'\',\''+val+'\')">'+val+'</a>')
+            }
+            table.append("<tr><th>"+i+"</th><td>"+val+"</td></tr>")
+        } else {
+            table.append("<tr><th>"+i+"</th><td>"+item.itemMeta[i]+"</td></tr>")
+        }
+    }
+}
+
+function changeNarrative(narrative, value) {
+    curNarrative = narrative;
+    curVal = value;
+    if (curNarrative == "place") {
+        $("body").css("background-image", "url(images/prova_bg_place.png)");
+    } if (curNarrative == "artistic expression") {
+        $("body").css("background-image", "url(images/prova_bg_genre.png)");
+    } if (curNarrative == "time") {
+        $("body").css("background-image", "url(images/prova_bg_time.png)");
+    }
+    prepareNarratives()
+}
+
+function prepareNavigationButtons(index) {
+    if (index > 0) {
+        $("#prevBtn").removeClass("disabled");
+        $("#prevBtn").click(function() {showInfo(index - 1)});
+        $("#prevBtn").text(currentSelection[index - 1].name)
+    } else {
+        $("#prevBtn").addClass("disabled");
+        $("#prevBtn").click = null;
+        $("#prevBtn").text("--");
+    }
+    if (index < currentSelection.length - 1) {
+        $("#nextBtn").removeClass("disabled");
+        $("#nextBtn").click(function() {showInfo(index + 1)});
+        $("#nextBtn").text(currentSelection[index + 1].name)
+    } else {
+        $("#nextBtn").addClass("disabled");
+        $("#nextBtn").click = null;
+        $("#nextBtn").text("--");
+    }
+    $("#narrative").text(curNarrative+": "+curVal)
+}
